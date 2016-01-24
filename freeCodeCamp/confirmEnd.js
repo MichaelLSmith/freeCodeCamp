58cc8a8cdb0be57e339e9b6d714c22b95544674d
Michael Laurence Smith <mlsmith45@gmail.com>
2016-01-24 02:55:52 +0000

    finished confirmEnd

--- freeCodeCamp/confirmEnd.js
+++ freeCodeCamp/confirmEnd.js
@@ -1,26 +1,20 @@
 function end(str, target) {
-  
-//   var length = str.length;
-  var array = str.split(' ');
-//   var outcome;
-  console.log(array[1]);
-//   console.log(array[array.length -1]);
-  
-  
-    if(array[1] == undefined){//if there is not a second word in the array (therefore if the second index position is undefined)
-    console.log('first if');
-        var stringLength = array[0].length -1;
-        console.log(array[0][stringLength]);
-            if (array[0][stringLength] === target){
-                console.log('second if');
-                return true; }
-                
-            else {return false;}
+    
+    var s = str.length -1; //s = index of last char of str
+    var result = false;
+    
+   
+    for (var t = target.length -1; t >= 0; t--){
+        if(target[t] === str[s]){ //if the char of both str and target at index t match
+            s -= 1;//decrease str index
+            result = true;
+        }
+        else { return false } //if the last two chars don't match, return false and end program.
     }
-    if(array[array.length -1] === target)
-            return true;
-  
-    else return false;
-}
 
-console.log(end("Walking on water and developing software from a specification are easy if both are frozen", "specification") );
+    if (result === true) { return true }
+            
+    else { return false }
+
+}
+console.log(end("He has to give me a new name", "me") );
