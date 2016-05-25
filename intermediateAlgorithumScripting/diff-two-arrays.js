133210bc3d81bf7589704ab89faf5a0f6869c65f
Michael Laurence Smith <mlsmith45@gmail.com>
2016-05-25 15:21:30 +0000

    firguring out diff2array

--- intermediateAlgorithumScripting/diff-two-arrays.js
+++ intermediateAlgorithumScripting/diff-two-arrays.js
@@ -1,22 +1,36 @@
-var t0 = performance.now();
-console.log(t0);
+// var t0 = performance.now();
+// console.log(t0);
 
 function diff(arr1, arr2) {
   // Same, same; but different.
+  
+  
+  //concatinate(combine the two arrays into one)
   var arr = arr1.concat(arr2);
+  console.log(arr);
+  
   var arrLen = arr.length;
-  var hash = {};
-  var dupes = [];
+  var hash = {};  //hash table to store values to compare
+  var dupes = []; //array to push duplicates
 
   for (var i = 0; i < arrLen; i++) 
   {
+      //check if property already exists in hash table
       if (hash.hasOwnProperty( arr[i]) )
       {
+          //if it does, push it to the dupes array
           dupes.push(arr[i]);
       }
       else {
+          //otherwise add it to the hash table
+          console.log('in else statement');
+          console.log(arr[i]);
+          console.log(hash[arr[i]]);
           hash[arr[i]] = arr[i];
       }
+      console.log('hash and dupes in for loop');
+      console.log(hash);
+      console.log(dupes);
   }
   function unique(item) {
       if(dupes.indexOf(item) === -1)
@@ -32,9 +46,9 @@ function diff(arr1, arr2) {
 
 console.log(diff([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));
 
-var t1 = performance.now();
-console.log(t1);
+// var t1 = performance.now();
+// console.log(t1);
 
-console.log( "Call to diff took " + (t1 - t0) + " milliseconds.");
+// console.log( "Call to diff took " + (t1 - t0) + " milliseconds.");
 // diff( ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] );
 
--- intermediateAlgorithumScripting/whereArtThou.js
+++ intermediateAlgorithumScripting/whereArtThou.js
@@ -7,7 +7,7 @@ function whereAreYou(collection, source) {
 
 // 1. During the collection loop, for each key/value pair, run the source loop.
 
-//      1.b for each collection obj k/v pair, check entire source obj.
+//  1.b for each collection obj k/v pair, check entire source obj.
 
 // 2. For each source object compare the active source key/value pair to the active collection k/v pair.
 
@@ -17,32 +17,17 @@ function whereAreYou(collection, source) {
 
 //If the 
 
+//Questions
+// 1. How do you know if you've reached the end of the source object?
 
-//Collection Array
+//Collection Loop
 // 1. loop through collection array of objects
 // 2. iterate through each object using a for in loop
 
     
-    //Source Object
-    // 1. Look through source obj
-    // 2. Save something in a variable??? is this necessary?
-
-
-
-// 3. If they match, continue to the next source k/v pair
-//Questions
-// 1. How do you know if you've reached the end of the source object?
-
-
-
 //Source Loop
-console.log('source Loop');
-console.log(source);
-for (var key in source){
-    if (source.hasOwnProperty(key)){
-                
-    }
-}
+// 1. Look through source obj
+// 2. Save something in a variable??? is this necessary?
 
 
 //Collection loop;
@@ -59,11 +44,21 @@ for (var i = 0; i < collection.length; i++) {
     }
 }
 
+//Source Loop
+console.log('source Loop');
+console.log(source);
+for (var key in source){
+    if (source.hasOwnProperty(key)){
+        console.log(key + "->" + c[key]);        
+    }
+}
+
+
   // Only change code above this line
 //   return arr;
 }
 
-whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+// whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
 
 // whereAreYou([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 });
 // //[{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }]
@@ -71,5 +66,5 @@ whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: nu
 // whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
 // //[{ "a": 1, "b": 2 }, { "a": 1, "b": 2, "c": 2 }]
 
-// whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 });
-// //[{ "a": 1, "b": 2, "c": 2 }]
+whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 });
+//[{ "a": 1, "b": 2, "c": 2 }]
