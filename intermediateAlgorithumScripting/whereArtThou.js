43cf9ca7511201382fea5459244a8d67a21fe216
Michael Laurence Smith <mlsmith45@gmail.com>
2016-05-28 19:53:34 +0000

    final whereArtThou

--- intermediateAlgorithumScripting/whereArtThou.js
+++ intermediateAlgorithumScripting/whereArtThou.js
@@ -1,64 +1,63 @@
 function whereAreYou(collection, source) {
   // What's in a name?
   var arr = [];
+  var indexArr = [];
   // Only change code below this line
-
-
-
-// 1. During the collection loop, for each key/value pair, run the source loop.
-
-//  1.b for each collection obj k/v pair, check entire source obj.
-
-// 2. For each source object compare the active source key/value pair to the active collection k/v pair.
-
-// 3. If the source obj does not have the active collection k/v pair, move onto the next collection obj.
-
-//4. If the source obj has the the active collection k/v pair, move on to the next source obj k/v pair
-
-//If the 
-
-//Questions
-// 1. How do you know if you've reached the end of the source object?
-
-//Collection Loop
-// 1. loop through collection array of objects
-// 2. iterate through each object using a for in loop
-
-    
-//Source Loop
-// 1. Look through source obj
-// 2. Save something in a variable??? is this necessary?
-
-
-//Collection loop;
-console.log('collection loop');
 
 //for loop to access each object in the collection array
 for (var i = 0; i < collection.length; i++) {
-    
+   
+
     // c holds each obj for each running of the collection loop
     var c = collection[i];
-    console.log('acitve collection k/v:');
-    console.log(c);
+   
 
-    //for loop to go through each obj
-    for (var key in c) {
-        if (c.hasOwnProperty(key)) {
-            console.log(key + "->" + c[key]);   
-        } 
+    //for loop to go through source obj
+    for (var key in source) {
+        
+        //if active collection obj does not contain the key of the current source key, note the index number of that obj
+        if (c.hasOwnProperty(key) === false) {
+            console.log('in if statement');
+            indexArr[i] = false;
+        }
+        //if it does have the property, check the value. If the value doesn't match add false to the indexArr
+       
+        if (c[key] === source[key]) {
+            indexArr[i] = true;
+        }
+        else { indexArr[i] = false }
+    console.log('end of for-in loop');
+    //end for-in loop for each active collection obj
+    }
+    console.log('index end of for loop:'+ i);
+    console.log('indexArr');
+    console.log(indexArr);
+    console.log('indexArr[i]');
+    console.log(indexArr[i]);
+    //at the end of checking entire active obj from collection, I need to know if there were any false hits.
+    if (indexArr[i] === undefined){
+    console.log('in hasOwnProperty if statement');
+    indexArr[i] = true;
+    console.log('indexObj in if statement');
+    console.log(indexArr);
     }
     
-    //Source Loop
-        console.log('source Loop');
-        console.log(source);
-        for (var key in source){
-            if (source.hasOwnProperty(key)){
-                console.log(key + "->" + source[key]);        
-            }
-        }
-        console.log('source loop end');
-        //source loop end
+    
+    
+//end for loop to go through collection array
 }
+
+console.log('index after for loop');
+console.log(i);
+//loop through indexArr to find the index of the true obj
+for (var m in indexArr) {
+    if (indexArr[m] === true){
+        arr.push(collection[m]);
+    }
+}
+
+console.log('arr');
+console.log(arr);
 
 
 
@@ -66,7 +65,8 @@ for (var i = 0; i < collection.length; i++) {
 //   return arr;
 }
 
-// whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+// [{ first: "Tybalt", last: "Capulet" }]
 
 // whereAreYou([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 });
 // //[{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }]
@@ -74,5 +74,5 @@ for (var i = 0; i < collection.length; i++) {
 // whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
 // //[{ "a": 1, "b": 2 }, { "a": 1, "b": 2, "c": 2 }]
 
-whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 });
+// whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 });
 //[{ "a": 1, "b": 2, "c": 2 }]
