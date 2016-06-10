39e4d2cd133b2ef91314bd2dbea591339e49b089
MichaelLSmith <mlsmith45@gmail.com>
2016-06-10 13:18:13 -0400

    better dna solution

--- intermediateAlgorithumScripting/algorithms/dna.js
+++ intermediateAlgorithumScripting/algorithms/dna.js
@@ -1,46 +1,24 @@
 console.log('dna.js');
 function pairElement(str) {
-    //AT, CG
+    var paired = [];
 
-    //split str into an array  [G,C,A]
-    //loop through str-array and pair each letter.
-    //create multi-dimensional array
-        //
-
-        // var str = ['G','C','G'];
-        str = str.split('');
-        console.log(str);
-        var strLen = str.length;
-        var innerArr = [];
-        var arr = [];
-        //AT, CG
-
-        for(var i = 0; i < strLen; i++){
-        	innerArr.push(str[i]);//[G]
-        	switch(str[i]){
-        		case 'G':
-        			innerArr.push('C');
-        		break;
-        		case 'C':
-        			innerArr.push('G');
-        		break;
-        		case 'A':
-        		  innerArr.push('T');
-        		break;
-        		case 'T':
-        			innerArr.push('A');
-        		break;
-        	}
-        	console.log('innerArr:');
-        	console.log(innerArr);
-        	arr.push(innerArr);
-        	console.log('arr:');
-        	console.log(arr);
-        	innerArr = [];
-        	console.log(innerArr);
-        }
-
-  return arr;
+    for(var i = 0; i < str.length; i++){
+    	switch(str[i]){
+    		case 'G':
+    			paired.push(['G', 'C']);
+    		break;
+    		case 'C':
+    			paired.push(['C', 'G']);
+    		break;
+    		case 'A':
+    		  paired.push(['A','T']);
+    		break;
+    		case 'T':
+    			paired.push(['T','A']);
+    		break;
+    	}
+    }
+  return paired;
 }
 
 // pairElement("GCG"); // should return [["G", "C"], ["C","G"],["G", "C"]]
@@ -50,24 +28,24 @@ function pairElement(str) {
 
 
 //test:
-console.log('test.js');
+// console.log('test.js');
+// //
+// //
+// (function () {
+//   'use strict';
 //
-//
-(function () {
-  'use strict';
-
-  describe('pairElement', function () {
-      it('translates GCG to [["G", "C"], ["C","G"],["G", "C"]]', function () {
-        expect(pairElement('GCG')).toEqual([["G", "C"], ["C","G"],["G", "C"]]);
-      });
-      it('translates ATCGA to [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]', function () {
-        expect(pairElement('ATCGA')).toEqual([["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]);
-      });
-      it('translates TTGAG to [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]]', function () {
-        expect(pairElement('TTGAG')).toEqual([["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]]);
-      });
-      it('translates CTCTA to [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]]', function () {
-        expect(pairElement('CTCTA')).toEqual([["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]]);
-      });
-  });
-})();
+//   describe('pairElement', function () {
+//       it('translates GCG to [["G", "C"], ["C","G"],["G", "C"]]', function () {
+//         expect(pairElement('GCG')).toEqual([["G", "C"], ["C","G"],["G", "C"]]);
+//       });
+//       it('translates ATCGA to [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]', function () {
+//         expect(pairElement('ATCGA')).toEqual([["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]);
+//       });
+//       it('translates TTGAG to [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]]', function () {
+//         expect(pairElement('TTGAG')).toEqual([["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]]);
+//       });
+//       it('translates CTCTA to [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]]', function () {
+//         expect(pairElement('CTCTA')).toEqual([["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]]);
+//       });
+//   });
+// })();
