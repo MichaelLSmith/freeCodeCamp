688ed704ce55625fcc84419eb14e3e31bf822e7f
MichaelLSmith <mlsmith45@gmail.com>
2016-06-19 09:34:42 -0400

    almost working missing letters

--- intermediateAlgorithumScripting/algorithms/missingLetters.js
+++ intermediateAlgorithumScripting/algorithms/missingLetters.js
@@ -4,10 +4,38 @@ function fearNotLetter(str) {
     //if char code of 2nd letter === 1st + 1, move on to the next letter
     //if it doesn't, find letter with char code of 1st + 1 and return it
 
-    var firstCode = str.charCodeAt(0);
-    var currentChar;
+    var currentCode,
+        nextCode,
+        missingCode =  undefined;
 
+    for (var i = 0; i < str.length - 1; i++) {
+        console.log('current Char: ' + str[i]);
+        currentCode = str.charCodeAt(i);
+        console.log('currentCode: ' + currentCode);
+        console.log('currentCode + 1:');
+        console.log(currentCode + 1);
+        nextCode = str.charCodeAt(i + 1);
+        console.log('nextCode: ' + nextCode)
+        if(currentCode + 1 !== nextCode) {
+            console.log('in if statement');
+            missingCode = nextCode;
+            console.log(missingCode);
+            console.log('end of 1st if statement');
+        }
+    }
 
+    console.log('missingCode: ' + missingCode);
+    // missingCode !== undefined ? str = String.fromCharCode(missingCode) : str = missingCode;
+
+    if(missingCode !== undefined) {
+        console.log('in 2nd if statement');
+        console.log('missingCode');
+        str = String.fromCharCode(missingCode);
+        console.log('end of 2nd if');
+    }
+    else { str = missingCode };
+
+    console.log(str);
 
   return str;
 }
--- intermediateAlgorithumScripting/test/spec/test.js
+++ intermediateAlgorithumScripting/test/spec/test.js
@@ -1,21 +1,20 @@
 console.log('test.js');
 //
-//
 (function () {
   'use strict';
 
   describe('fearNotLetter', function () {
     it('abce returns d', function () {
-        expect(pairElement('abce')).toEqual('d');
+        expect(fearNotLetter('abce')).toEqual('d');
     });
-    it('abcdefghjklmno returns i', function () {
-      expect(pairElement('abcdefghjklmno')).toEqual('i');
-    });
-    it('bcd returns undefined', function () {
-      expect(pairElement('bcd')).toEqual(undefined);
-    });
-    it('yz returns undefined', function () {
-      expect(pairElement('yz')).toEqual(undefined);
-    });
+    // it('abcdefghjklmno returns i', function () {
+    //   expect(fearNotLetter('abcdefghjklmno')).toEqual('i');
+    // });
+    // it('bcd returns undefined', function () {
+    //   expect(fearNotLetter('bcd')).toEqual(undefined);
+    // });
+    // it('yz returns undefined', function () {
+    //   expect(fearNotLetter('yz')).toEqual(undefined);
+    // });
   });
 })();
