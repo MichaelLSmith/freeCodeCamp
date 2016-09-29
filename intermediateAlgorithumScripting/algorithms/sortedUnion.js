15195345b92e30f10bc6f58a83f6914ab198f444
MichaelLSmith <mlsmith45@gmail.com>
2016-09-29 17:44:17 -0400

    finished sortedUnion

--- intermediateAlgorithumScripting/algorithms/sortedUnion.js
+++ intermediateAlgorithumScripting/algorithms/sortedUnion.js
@@ -1,61 +1,96 @@
-// var flattened = [[0, 1], [2, 3], [4, 5]].reduce(function(a, b) {
-//     // console.log('index: '+i);
-//     // console.log(a);
-//     // console.log(b);
-//   return a.concat(b);
-// }, []);
-// // flattened is [0, 1, 2, 3, 4, 5]
-// console.log(flattened);
-
 // jshint esversion:6
 function uniteUnique(arr) {
     // console.log(arguments);
     let args = [...arguments];
     console.log(args);
+    let hash = {};//hash table to store values to compare
+    let output = []; //consider using map or filter to create new array automatically.
 
-    let output = args.reduce(function(p,c,i){
-        // console.log('start');
-        // console.log('index: ' + i);
-        // console.log('current:');
-        // console.log(c);
-        // console.log('previous');
-        // console.log(p);
-        return p.concat(c);
-
-        },[]
-    );
-    // output.sort();
-    console.log('output: ');
-    console.log(output);
-    // output = output.filter(function(elm, i){
-    //     console.log('start');
-    //     console.log('index: ' + i);
-    //     console.log('elm:');
-    //     console.log(elm);
-    //     console.log(elm -1);
-    //     console.log(output);
-    //     return elm !== i -1;
-    // },0);
-
-    let array = [];
-    for(let i = 0; i < output.length; i++){
-        // console.log(i);
-        console.log(output[i]);
-        console.log(output[i-1]);
-
-        if(output[i] !== output[i -1] || output[i -1] === undefined){
-            console.log('in if statement');
-            array.push(output[i]);
+    /*
+    forEach or map??? or filter
+    1. go through each value of args array. Each of these in an internal array
+    2. do another forEach on each internal array
+    3. If value is an array {
+            if ivalue[0] is in hash table {
+                do nothing to output array
+            }
+            else (if ivalue[0] is not in hash table) {
+                add value to hash table
+                push value to output array
+            }
         }
+        Else (if value isn't an array) {
+            if ivalue is in hash table {
+                do nothing to output array
+            }
+            else (if ivalue is not in hash table) {
+                add value to hash table
+                push value to output array
+            }
+        }
+    */
 
-    }
+    //forEach for the args array
+    args.forEach( (v, i) => {
+        console.log('index: ');
+        console.log(i);
+        console.log('value:');
+        console.log(v);
 
-    console.log(array);
+        v.forEach( (ivalue, iIndex ) => {
+            console.log('Inner index: ');
+            console.log(iIndex);
+            console.log('Inner value:');
+            console.log(ivalue);
+            console.log('hash: ');
+            console.log(hash);
 
-    return arr;
+            //check if value is array:
+            if(Array.isArray(ivalue)){
+                console.log('in if check array');
+                //check if value at first index (asumes all arrays only hold only one value)
+                if(hash.hasOwnProperty( ivalue[0] )){
+                    console.log('in check has on array value');
+                    console.log('do nothing');
+                    output.push(ivalue);
+                }
+                /*
+                if it is array, but value doesn't exist in hash table
+                1. add value of array first index to hash table
+                2. push value to output array.
+                */
+                else {
+                    console.log('in else for array but not in hash');
+                    hash[ivalue[0]] = ivalue[0];
+                    output.push(ivalue);
+                }
+            }
+            //if value isn't an array, check if value exists in hash table
+            else {
+                if (hash.hasOwnProperty( ivalue )){
+                    console.log('in not array, value exists');
+                    console.log('do nothing');
+                }
+                /*
+                if it does exist
+                1. Add value to hash table
+                2. do nothing to the output array
+                */
+                else {
+                    console.log('in not array, value doesnt exist');
+                    hash[ivalue] = ivalue;
+                    //then push that value to output array.
+                    output.push(ivalue);
+                }
+            }
+            console.log('output in Inner forEach')
+            console.log(output);
+        });
+    });
+
+    console.log(output);
 }
-
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) //should return [1, 3, 2, 5, 4].
-// uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) //should return [1, 3, 2, [5], [4]].
+// uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) //should return [1, 3, 2, 5, 4].
+uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) //should return [1, 3, 2, [5], [4]].
 // uniteUnique([1, 2, 3], [5, 2, 1]) //should return [1, 2, 3, 5].
 // uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) //should return [1, 2, 3, 5, 4, 6, 7, 8].
--- intermediateAlgorithumScripting/algorithms/working.js
+++ intermediateAlgorithumScripting/algorithms/working.js
@@ -1,20 +1,99 @@
-function destroyer(arr) {
-    console.log(arr);
-    //original way of converting arguments into an array; can now
-    //use spread operator (...arguments).
-    var checks = Array.prototype.slice.call(arguments);
-    console.log(checks);
-    //splices out the first array of values in the arguments, just leaving the remaining arguments to check against.
-    checks.splice(0,1);
-    console.log(checks);
-    console.log(arr);//arr just has the array of values to check against because the other two arguments passed into the function aren't included in arr variable. They are only accessible through the arguments obj.
-    return arr.filter(function(v,i){
-        //goes through the original array argument and checks each value
-        return checks.indexOf(v) === -1;
-    });
+// jshint esversion:6
+function uniteUnique(arr) {
+    // console.log(arguments);
+    let args = [...arguments];
+    console.log(args);
+    let hash = {};//hash table to store values to compare
+    let output = []; //consider using map or filter to create new array automatically.
+
+    /*
+    forEach or map??? or filter
+    1. go through each value of args array. Each of these in an internal array
+    2. do another forEach on each internal array
+    3. If value is an array {
+            if ivalue[0] is in hash table {
+                do nothing to output array
+            }
+            else (if ivalue[0] is not in hash table) {
+                add value to hash table
+                push value to output array
+            }
+        }
+        Else (if value isn't an array) {
+            if ivalue is in hash table {
+                do nothing to output array
+            }
+            else (if ivalue is not in hash table) {
+                add value to hash table
+                push value to output array
+            }
+        }
+    */
+
+    //forEach for the args array
+    args.forEach( (v, i) => {
+        console.log('index: ');
+        console.log(i);
+        console.log('value:');
+        console.log(v);
+
+        v.forEach( (ivalue, iIndex ) => {
+            console.log('Inner index: ');
+            console.log(iIndex);
+            console.log('Inner value:');
+            console.log(ivalue);
+            console.log('hash: ');
+            console.log(hash);
+
+            //check if value is array:
+            if(Array.isArray(ivalue)){
+                console.log('in if check array');
+                //check if value at first index (asumes all arrays only hold only one value)
+                if(hash.hasOwnProperty( ivalue[0] )){
+                    console.log('in check has on array value');
+                    console.log('do nothing');
+                    output.push(ivalue);
+                }
+                /*
+                if it is array, but value doesn't exist in hash table
+                1. add value of array first index to hash table
+                2. push value to output array.
+                */
+                else {
+                    console.log('in else for array but not in hash');
+                    hash[ivalue[0]] = ivalue[0];
+                    output.push(ivalue);
+                }
+            }
+            //if value isn't an array, check if value exists in hash table
+            else {
+                if (hash.hasOwnProperty( ivalue )){
+                    console.log('in not array, value exists');
+                    console.log('do nothing');
+                }
+                /*
+                if it does exist
+                1. Add value to hash table
+                2. do nothing to the output array
+                */
+                else {
+                    console.log('in not array, value doesnt exist');
+                    hash[ivalue] = ivalue;
+                    //then push that value to output array.
+                    output.push(ivalue);
+                }
+            }
+            console.log('output in Inner forEach')
+            console.log(output);
+        });
+        })
+
+    console.log(output);
+
+    return arr;
+
 }
-
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);//return [1, 1];
-
-
-/*You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.*/
+// uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) //should return [1, 3, 2, 5, 4].
+uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) //should return [1, 3, 2, [5], [4]].
+// uniteUnique([1, 2, 3], [5, 2, 1]) //should return [1, 2, 3, 5].
+// uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) //should return [1, 2, 3, 5, 4, 6, 7, 8].
