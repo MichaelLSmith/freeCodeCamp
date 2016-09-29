0f6fc65c682bc70cf7352c4b0988b95736417d5a
MichaelLSmith <mlsmith45@gmail.com>
2016-09-29 11:36:49 -0400

    added explanation of BooWho official solution

--- intermediateAlgorithumScripting/algorithms/booWho.js
+++ intermediateAlgorithumScripting/algorithms/booWho.js
@@ -22,11 +22,15 @@ booWho(true);
 http://forum.freecodecamp.com/t/algorithm-boo-who/16000
 
 */
-
-
 function booWho(bool) {
   return typeof bool === 'boolean';
 }
 
 // test here
 booWho(null);
+
+/*explaination of why this works:
+an expression of comparison will print out a boolean value of true or false
+*/
+console.log(1 === 2); // false
+console.log(1 === 1); // true
